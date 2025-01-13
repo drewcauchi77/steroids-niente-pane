3a04f6d64a56f28ac02d7b2555960763ca6cb819
Andrew Cauchi <drewcauchi@gmail.com>
2025-01-13 23:11:36 +0100

    70

---
+++ 76.js
@@ -0,0 +1,88 @@
+/**
+ * SLIDING WINDOW (!!!)
+ *
+ * 76. Minimum Window Substring
+ * 
+ * Given two strings `s` and `t` of lengths `m` and `n` respectively, 
+ * return the minimum window substring of `s` such that every character in `t` (including duplicates) 
+ * is included in the window. If there is no such substring, return the empty string "".
+ * 
+ * The test cases will be generated such that the answer is unique.
+ * 
+ * Example 1:
+ * Input: s = "ADOBECODEBANC", t = "ABC"
+ * Output: "BANC"
+ * Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string `t`.
+ * 
+ * Example 2:
+ * Input: s = "a", t = "a"
+ * Output: "a"
+ * Explanation: The entire string `s` is the minimum window.
+ * 
+ * Example 3:
+ * Input: s = "a", t = "aa"
+ * Output: ""
+ * Explanation: Both 'a's from `t` must be included in the window. 
+ * Since the largest window of `s` only has one 'a', return empty string.
+ * 
+ * Constraints:
+ * - m == s.length
+ * - n == t.length
+ * - 1 <= m, n <= 10^5
+ * - `s` and `t` consist of uppercase and lowercase English letters.
+ * 
+ * Follow-up: Could you find an algorithm that runs in O(m + n) time?
+ */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+const minWindow = (s, t) => {
+    if (s.length < t.length) {
+        return "";
+    }
+
+    const charCount = new Map();
+    for (const ch of t) {
+        charCount.set(ch, (charCount.get(ch) || 0) + 1);
+    }
+
+    let targetCharsRemaining = t.length;
+    let minWindow = [0, Number.POSITIVE_INFINITY];
+    let startIndex = 0;
+
+    for (let endIndex = 0; endIndex < s.length; endIndex++) {
+        const ch = s[endIndex];
+        if (charCount.has(ch) && charCount.get(ch) > 0) {
+            targetCharsRemaining--;
+        }
+        charCount.set(ch, (charCount.get(ch) || 0) - 1);
+
+        if (targetCharsRemaining === 0) {
+            while (true) {
+                const charAtStart = s[startIndex];
+                if (charCount.has(charAtStart) && charCount.get(charAtStart) === 0) {
+                    break;
+                }
+                charCount.set(charAtStart, (charCount.get(charAtStart) || 0) + 1);
+                startIndex++;
+            }
+
+            if (endIndex - startIndex < minWindow[1] - minWindow[0]) {
+                minWindow = [startIndex, endIndex];
+            }
+
+            charCount.set(s[startIndex], (charCount.get(s[startIndex]) || 0) + 1);
+            targetCharsRemaining++;
+            startIndex++;
+        }
+    }
+
+    return minWindow[1] >= s.length ? "" : s.slice(minWindow[0], minWindow[1] + 1);
+}
+
+console.log(minWindow("ADOBECODEBANC", "ABC")); // "BANC"
+console.log(minWindow("a", "a")); // "a"
+console.log(minWindow("a", "aa")); // ""
