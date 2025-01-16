169195bebfbf28ddc7d08c9e624b8b05e0a4fb37
Andrew Cauchi <drewcauchi@gmail.com>
2025-01-16 23:20:55 +0100

    36, 217

---
+++ 36.js
@@ -0,0 +1,105 @@
+/**
+ * ARRAYS
+ *
+ * 36. Valid Sudoku
+ * 
+ * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ * 
+ * 1. Each row must contain the digits 1-9 without repetition.
+ * 2. Each column must contain the digits 1-9 without repetition.
+ * 3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * 
+ * Note:
+ * - A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * - Only the filled cells need to be validated according to the mentioned rules.
+ * 
+ * Example 1:
+ * Input: board = 
+ * [["5","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: true
+ * 
+ * Example 2:
+ * Input: board = 
+ * [["8","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
+ * Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ * 
+ * Constraints:
+ * - board.length == 9
+ * - board[i].length == 9
+ * - board[i][j] is a digit 1-9 or '.'.
+ */
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+const isValidSudoku = (board) => {
+    const rows = new Map();
+    const cols = new Map();
+    const boxes = new Map();
+
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            const value = board[row][col];
+
+            if (value !== ".") {
+                if (!rows.has(row)) rows.set(row, new Set()); // row is index
+                if (rows.get(row).has(value)) return false;
+                rows.get(row).add(value);
+
+                if (!cols.has(col)) cols.set(col, new Set());
+                if (cols.get(col).has(value)) return false;
+                cols.get(col).add(value);
+
+                const boxIndex = Math.floor(row / 3) * 3 + Math.floor(col / 3);
+
+                if (!boxes.has(boxIndex)) boxes.set(boxIndex, new Set());
+                if (boxes.get(boxIndex).has(value)) return false;
+                boxes.get(boxIndex).add(value);
+            }
+        }
+    }
+
+    return true;
+};
+
+console.log(isValidSudoku([
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+])); // true
+console.log(isValidSudoku([
+    ["8","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+])); // false
